820ed5fb8a6f21ded204e8e5303cd6b014f771be
Dami <111752353+dami-moj@users.noreply.github.com>
2024-01-12 12:53:28 +0000

    testing docker

---
+++ 1_basics_n_setup/2_docker_sql/test_docker.py
@@ -0,0 +1,13 @@
+import sys
+import pandas as pd
+
+# some fancy stuff with pandas
+print(sys.argv)
+
+day = sys.argv[1]
+
+val = 25 + 35
+
+print(f'Check python the summation is {val} for day = {day}')
+
+
